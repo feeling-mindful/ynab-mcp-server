16494f9455cd489fd24917b0c1d067e1bfd56893
Nick Prokesch <nick@prokes.ch>
2026-05-10 15:55:54 -0500

    feat(http): add all missing tools to streaming HTTP route
    Syncs app/[transport]/route.ts with src/index.ts. Previously 17 tools
    were missing from the HTTP server that Perplexity connects to.
    
    Added:
    - Budgets: GetBudgetById, GetBudgetSettings
    - Accounts: GetAccountById, CreateAccount
    - Payees: GetPayeeById, CreatePayee, UpdatePayee, ListPayeeLocations,
      GetPayeeLocationById, GetPayeeLocationsByPayee
    - Scheduled Transactions: GetScheduledTransactionById,
      CreateScheduledTransaction, UpdateScheduledTransaction,
      DeleteScheduledTransaction
    - User: GetUser
    - Money Movements: GetMoneyMovements, GetMoneyMovementGroups

--- app/[transport]/route.ts
+++ app/[transport]/route.ts
@@ -6,18 +6,41 @@ import * as ynab from "ynab";
 import * as ApproveTransactionTool from "../../src/tools/ApproveTransactionTool.js";
 import * as BudgetSummaryTool from "../../src/tools/BudgetSummaryTool.js";
 import * as BulkApproveTransactionsTool from "../../src/tools/BulkApproveTransactionsTool.js";
+import * as CreateAccountTool from "../../src/tools/CreateAccountTool.js";
+import * as CreateCategoryGroupTool from "../../src/tools/CreateCategoryGroupTool.js";
+import * as CreateCategoryTool from "../../src/tools/CreateCategoryTool.js";
+import * as CreatePayeeTool from "../../src/tools/CreatePayeeTool.js";
+import * as CreateScheduledTransactionTool from "../../src/tools/CreateScheduledTransactionTool.js";
 import * as CreateTransactionTool from "../../src/tools/CreateTransactionTool.js";
+import * as DeleteScheduledTransactionTool from "../../src/tools/DeleteScheduledTransactionTool.js";
 import * as DeleteTransactionTool from "../../src/tools/DeleteTransactionTool.js";
+import * as GetAccountByIdTool from "../../src/tools/GetAccountByIdTool.js";
+import * as GetBudgetByIdTool from "../../src/tools/GetBudgetByIdTool.js";
+import * as GetBudgetSettingsTool from "../../src/tools/GetBudgetSettingsTool.js";
+import * as GetCategoryByIdTool from "../../src/tools/GetCategoryByIdTool.js";
+import * as GetMoneyMovementGroupsTool from "../../src/tools/GetMoneyMovementGroupsTool.js";
+import * as GetMoneyMovementsTool from "../../src/tools/GetMoneyMovementsTool.js";
+import * as GetMonthCategoryByIdTool from "../../src/tools/GetMonthCategoryByIdTool.js";
+import * as GetPayeeByIdTool from "../../src/tools/GetPayeeByIdTool.js";
+import * as GetPayeeLocationByIdTool from "../../src/tools/GetPayeeLocationByIdTool.js";
+import * as GetPayeeLocationsByPayeeTool from "../../src/tools/GetPayeeLocationsByPayeeTool.js";
+import * as GetScheduledTransactionByIdTool from "../../src/tools/GetScheduledTransactionByIdTool.js";
 import * as GetTransactionsTool from "../../src/tools/GetTransactionsTool.js";
 import * as GetUnapprovedTransactionsTool from "../../src/tools/GetUnapprovedTransactionsTool.js";
+import * as GetUserTool from "../../src/tools/GetUserTool.js";
 import * as ImportTransactionsTool from "../../src/tools/ImportTransactionsTool.js";
 import * as ListAccountsTool from "../../src/tools/ListAccountsTool.js";
 import * as ListBudgetsTool from "../../src/tools/ListBudgetsTool.js";
 import * as ListCategoriesTool from "../../src/tools/ListCategoriesTool.js";
 import * as ListMonthsTool from "../../src/tools/ListMonthsTool.js";
+import * as ListPayeeLocationsTool from "../../src/tools/ListPayeeLocationsTool.js";
 import * as ListPayeesTool from "../../src/tools/ListPayeesTool.js";
 import * as ListScheduledTransactionsTool from "../../src/tools/ListScheduledTransactionsTool.js";
 import * as UpdateCategoryBudgetTool from "../../src/tools/UpdateCategoryBudgetTool.js";
+import * as UpdateCategoryGroupTool from "../../src/tools/UpdateCategoryGroupTool.js";
+import * as UpdateCategoryTool from "../../src/tools/UpdateCategoryTool.js";
+import * as UpdatePayeeTool from "../../src/tools/UpdatePayeeTool.js";
+import * as UpdateScheduledTransactionTool from "../../src/tools/UpdateScheduledTransactionTool.js";
 import * as UpdateTransactionTool from "../../src/tools/UpdateTransactionTool.js";
 
 interface ToolModule {
@@ -29,22 +52,54 @@ interface ToolModule {
 }
 
 const tools: ToolModule[] = [
+  // Budgets
   ListBudgetsTool,
-  GetUnapprovedTransactionsTool,
+  GetBudgetByIdTool,
+  GetBudgetSettingsTool,
   BudgetSummaryTool,
-  CreateTransactionTool,
-  ApproveTransactionTool,
-  UpdateCategoryBudgetTool,
-  UpdateTransactionTool,
-  BulkApproveTransactionsTool,
-  ListPayeesTool,
+  // Transactions
   GetTransactionsTool,
+  GetUnapprovedTransactionsTool,
+  CreateTransactionTool,
+  UpdateTransactionTool,
   DeleteTransactionTool,
-  ListCategoriesTool,
-  ListAccountsTool,
-  ListScheduledTransactionsTool,
+  ApproveTransactionTool,
+  BulkApproveTransactionsTool,
   ImportTransactionsTool,
+  // Categories
+  ListCategoriesTool,
+  GetCategoryByIdTool,
+  GetMonthCategoryByIdTool,
+  CreateCategoryTool,
+  CreateCategoryGroupTool,
+  UpdateCategoryTool,
+  UpdateCategoryGroupTool,
+  UpdateCategoryBudgetTool,
+  // Accounts
+  ListAccountsTool,
+  GetAccountByIdTool,
+  CreateAccountTool,
+  // Payees
+  ListPayeesTool,
+  GetPayeeByIdTool,
+  CreatePayeeTool,
+  UpdatePayeeTool,
+  ListPayeeLocationsTool,
+  GetPayeeLocationByIdTool,
+  GetPayeeLocationsByPayeeTool,
+  // Scheduled Transactions
+  ListScheduledTransactionsTool,
+  GetScheduledTransactionByIdTool,
+  CreateScheduledTransactionTool,
+  UpdateScheduledTransactionTool,
+  DeleteScheduledTransactionTool,
+  // Months
   ListMonthsTool,
+  // User
+  GetUserTool,
+  // Money Movements
+  GetMoneyMovementsTool,
+  GetMoneyMovementGroupsTool,
 ] as ToolModule[];
 
 const handler = createMcpHandler(
